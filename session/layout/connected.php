b6edc47813b03ab77f0f9769ea870788e2fb4e40
Mathis-Gasparotto <78808807+Mathis-Gasparotto@users.noreply.github.com>
2021-03-24 12:03:57 +0100

    Tous les sites développés en cours de PHP

--- session/layout/connected.php
+++ session/layout/connected.php
@@ -1,8 +1,15 @@
 <div id="connected">
   <?php
-  if (isset($_SESSION['login'])) { ?>
+  if (isset($_SESSION['id'])) {
+    $dsn = "mysql:dbname=mds_php;host=127.0.0.1;charset=utf8mb4";
+    $pdo = new PDO($dsn, "mds_php", "NL6Syr5R18fZbdao");
+    $stmt = $pdo->prepare("SELECT * FROM client WHERE id =?");
+    $stmt->execute([$_SESSION['id']]);
+  }
+
+  if (isset($_SESSION['id'])) { ?>
     <div class="cercle_vert"></div>
-    <a href="logout.php">Disconnect (<?php echo $_SESSION['login']; ?>)</a>
+    <a href="logout.php">Disconnect (<?php echo $_SESSION['username']; ?>)</a>
   <?php } else { ?>
     <div class="cercle_rouge"></div>
     <a href="sign_in.php">Sign In</a>
--- session/sign_in.php
+++ session/sign_in.php
@@ -1,58 +1,58 @@
 <?php
-//Lorsqu'uen session n'est pas démarée et qu'on appelle session_start()
+//Lorsqu'une session n'est pas démarée et qu'on appelle session_start()
 //PHP va chercher s'il existe un PHPSESSID, donc un identifiant de session, dans les cookies.
 //S'il ne trouve pas d'identifiant, donc de PHPSESSID, alors il va en créer un, et le renvoyer au client dans un cookie.
 //S'il trouve un identifaint de session, donc un cookie PHPSESSID, alors il va restituer le contexte de la session dans le tabelau $_SESSION
 //Par défaut, le tableau $_SESSION sera vide
 //Nous pourrons écrire des informations dans $_SESSION, contrairement 
-?>
 
-<?php
 include_once "layout/header.php";
-require_once "model/users.php";
-?>
+// require_once "model/users.php";
 
-<form method="post">
-  <input type="text" name="login" placeholder="Login" required />
-
-  <input type="password" name="password" placeholder="Password" required />
-
-  <input type="submit" value="Connect" name="connect_submit">
-</form>
-
-<?php
+$dsn = "mysql:dbname=mds_php;host=127.0.0.1;charset=utf8mb4";
+$pdo = new PDO($dsn, "mds_php", "NL6Syr5R18fZbdao");
 
 if (isset($_POST['connect_submit']) && session_status() == PHP_SESSION_ACTIVE) {
 
   if (!empty($_POST['login']) && !empty($_POST['password'])) {
 
-    $goodLogin = false;
-    $goodPassword = false;
-
-    foreach ($users as $user) {
-
-      if ($_POST['login'] == $user['email'] && $goodLogin == false) {
-        $goodLogin = true;
-        if ($_POST['password'] == $user['pass'] && $goodPassword == false) {
-          $goodPassword = true;
-        } else {
-          $formError = "password incorrect";
-        }
-      } else {
-        $formError = "login incorrect";
-      }
-
-      if ($goodLogin && $goodPassword) {
-        $_SESSION['login'] = $user['firstname'];
-      }
+    $stmt = $pdo->prepare("SELECT id, firstname FROM client WHERE email=:user AND pass=:pass");
+    $result = $stmt->execute([
+      'user' => $_POST['login'],
+      'pass' => $_POST['password']
+    ]);
+    if (!$result) {
+      $formError = "login or password incorrect";
+    } else {
+      $client = $stmt->fetch(PDO::FETCH_ASSOC);
+      unset($formError);
+      $_SESSION['id'] = $client['id'];
+      $_SESSION['username'] = $client['firstname'];
+      header("Location: index.php");
+      exit;
     }
-  } else {
-    $formError = "password or login are empty";
   }
-}
 
-if (isset($_SESSION['login'])) {
-  header("Location: index.php");
+
+  // foreach ($users as $user) {
+
+  // if ($_POST['login'] == $user['email'] && $goodLogin == false) {
+  //   $goodLogin = true;
+  //   if ($_POST['password'] == $user['pass'] && $goodPassword == false) {
+  //     $goodPassword = true;
+  //   } else {
+  //     $formError = "password incorrect";
+  //   }
+  // } else {
+  //   $formError = "login incorrect";
+  // }
+
+  // if ($goodLogin && $goodPassword) {
+  //   $_SESSION['login'] = $user['firstname'];
+  // }
+  // }
+} else {
+  $formError = "password or login are empty";
 }
 
 if (isset($formError)) { ?>
@@ -62,6 +62,14 @@ if (isset($formError)) { ?>
 $_SESSION['page_counter'] = 1;
 
 ?>
+
+<form method="post">
+  <input type="text" name="login" placeholder="Login" required />
+
+  <input type="password" name="password" placeholder="Password" required />
+
+  <input type="submit" value="Connect" name="connect_submit">
+</form>
 
 
 <?php include_once "layout/footer.php"; ?>
