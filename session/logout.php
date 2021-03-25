d65c23145cdaf9c375a28d208e0e119e5c9b89e5
Mathis-Gasparotto <78808807+Mathis-Gasparotto@users.noreply.github.com>
2021-03-25 12:07:04 +0100

    Tous les sites développés en cours de PHP

--- session/logout.php
+++ session/logout.php
@@ -1,11 +1,12 @@
-<?php 
+<?php
 
 include_once "layout/header.php";
 
 /* unset($_SESSION['login']);
 unset($_SESSION['page_counter']); */
 
-$_SESSION = [];
+unset($_SESSION['id']);
+unset($_SESSION['page_counter']);
 
 header("Location: sign_in.php");
 exit;
